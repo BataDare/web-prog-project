b6c0cd61ad6b8ac7f23a43cf5161e1d99c64b4f9
BataDare <dareministar98@gmail.com>
2021-01-25 14:40:04 +0100

    draw proizvod and edit function

---
+++ root/proizvod.js
@@ -0,0 +1,63 @@
+export class Proizvod
+{
+    constructor(id, sifra, ime, cena, kolicina)
+    {
+        this.id = id;
+        this.sifra = sifra;
+        this.ime = ime;
+        this.cena = cena;
+        this.kolicina = kolicina;
+    }
+
+    crtajRed(tab)
+    {
+        const red = document.createElement("tr");
+        tab.appendChild(red);
+        let el = document.createElement("td");
+        el.innerHTML = this.sifra;
+        red.appendChild(el);
+
+        el = document.createElement("td");
+        el.innerHTML = this.ime;
+        red.appendChild(el);
+
+        el = document.createElement("td");
+        el.innerHTML = this.cena;
+        red.appendChild(el);
+
+        el = document.createElement("td");
+        el.innerHTML = this.kolicina;
+        el.className = "kolicina";
+        red.appendChild(el);
+
+        el = document.createElement("button");
+        el.innerHTML = "Kupi";
+        red.appendChild(el);
+        el.onclick = (ev) => 
+        {
+           this.kupiProizvod(red);
+        }
+    }
+
+    kupiProizvod(red)
+    {
+        var kolicinaTemp = this.kolicina;
+
+        if (kolicinaTemp > 0)
+        {
+            kolicinaTemp--;
+        }
+
+        fetch("https://localhost:5000/PROJEKAT/Izmeni/" + this.id + "/" + kolicinaTemp, {
+            method: "PUT"
+        }).then(p => {
+            if (p.ok)
+            {
+            this.kolicina = kolicinaTemp;
+
+            const domKolicina = red.querySelector(".kolicina");
+            domKolicina.innerHTML = this.kolicina;
+            }
+        })
+    }
+}
